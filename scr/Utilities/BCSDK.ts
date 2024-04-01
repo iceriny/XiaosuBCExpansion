619bbe90312d0ede9c3eef6b85261f4ec562944a
iceriny <suqianchengice@gmail.com>
2024-04-01 14:28:19 +0800

    Update SDK to v1.2.0

--- scr/Utilities/BCSDK.ts
+++ scr/Utilities/BCSDK.ts
@@ -9,7 +9,9 @@ export const bcModSDK = bcModSDKRef.registerMod({
     version: XSBE_VERSION.startsWith("v") ? XSBE_VERSION.slice(1) : XSBE_VERSION,
     repository: ModInfo.REPOSITORY_URL
 });
-export function hookFunction(target: string, priority: HookPriority, hook:PatchHook): () => void {
+//(functionName: TFunctionName, priority: number, hook: PatchHook<GetDotedPathType<typeof globalThis, TFunctionName>>)
+export function hookFunction(target: string, priority: HookPriority, hook: PatchHook): () => void {
+    // @ts-expect-error: 兼容1.2.0modSDK
     const removeCallback = bcModSDK.hookFunction(target, priority, hook);
     return removeCallback;
 }
@@ -18,7 +20,7 @@ export function patchFunction(functionName: string, patches: Record<string, stri
     bcModSDK.patchFunction(functionName, patches);
 }
 
-export function removePatches(functionName: string){
+export function removePatches(functionName: string) {
     bcModSDK.removePatches(functionName);
 }
 
